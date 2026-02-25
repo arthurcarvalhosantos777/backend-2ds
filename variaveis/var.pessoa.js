7f9b5843f14c028831b81a8db50a18e013569e32
arthurcarvalhosantos777 <arthur.carvalho.santos@escola.pr.gov.br>
2026-02-25 13:30:23 +0000

    variaveis para pessoas

---
+++ variaveis/var.pessoa.js
@@ -0,0 +1,15 @@
+let nome = "Arthur"
+let sobrenome = "Carvalho"
+const cpf = "234.453.756-XX"
+const rg = "53.345.654-X"
+let idade = 15
+let altura = 181
+let peso = 61
+
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(altura)
+console.log(peso)
