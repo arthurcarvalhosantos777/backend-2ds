d74824428461b8af6580bb0cbb4d8772caaa5173
arthurcarvalhosantos777 <arthur.carvalho.santos@escola.pr.gov.br>
2026-06-09 13:41:14 +0000

    introdução ao Módulo System - fs

---
+++ 2°Tri/modulos/fs/festa.md
@@ -0,0 +1,8 @@
+### Convidados da Minha Festa:
+**amigos especiais**
+- Luan
+- Jorge
+- Mariah
+- Louro José
+
+*Entradas apenas com ingressos Eventin*
---
+++ 2°Tri/modulos/fs/mod-fs.js
@@ -0,0 +1,4 @@
+const fs = require('fs')
+//              módulo-fs       diretório    arquivo    codificação  
+const arquivo = fs.readFileSync(__dirname + '/festa.md', 'utf-8')
+console.log(arquivo)
