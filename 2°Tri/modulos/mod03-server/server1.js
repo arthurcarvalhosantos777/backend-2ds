fb65534a1cabfde61acdf8f957251dcf64e44219
arthurcarvalhosantos777 <arthur.carvalho.santos@escola.pr.gov.br>
2026-08-05 11:46:16 +0000

    Adiciona arquivos HTML e CSS para a página inicial e sobre, além de implementar o servidor HTTP

---
+++ 2°Tri/modulos/mod03-server/server1.js
@@ -0,0 +1,34 @@
+const http = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 7777
+// Carrega o caminho dos arquivos
+const home = path.join(__dirname, 'Pages/index.html')
+const sobre = path.join(__dirname, 'Pages/sobre.html')
+
+const server = http.createServer((req, res) => {
+   // qual informação eu tenho?
+   const urlTratada = new URL(req.url, `http://${req.headers.host}`) //limpa a url e transforma em objeto
+   const recurso =  urlTratada.pathname //em qual rotas estamos?
+
+   if(recurso === '/') { //Página inicial
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.end(fs.readFileSync(home, 'utf-8'));
+   }
+   if(recurso === '/sobre') {
+    res.statusCode = 200; //Sobre nós
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    return res.end(fs.readFileSync(sobre, 'utf-8'));
+   }
+   else {  
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end('401 Não autorizado');
+   }
+
+});
+
+server.listen(porta, () => {
+  console.log(`Servidor rodando na porta ${porta}`)
+})
